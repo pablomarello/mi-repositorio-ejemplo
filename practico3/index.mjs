94eacac654db9e095527e9b4525cbbef2253301b
pablomarello <plmarello@gmail.com>
2024-10-25 15:59:02 -0300

    agregue practico3

---
+++ practico3/index.mjs
@@ -0,0 +1,13 @@
+import { agregarSuperheroes, leerSuperheroes } from './utils.mjs';
+
+//Leer y mostrar la lista de superheroes ordenada
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
+
+//Agregar nuevos superhéroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+//Leer y mostrar la lista actualizada de superhéroes ordenada
+const superheroes = leerSuperheroes(archivoOriginal);
+console.log('Superhéroes ordenados');
+console.log(superheroes);
